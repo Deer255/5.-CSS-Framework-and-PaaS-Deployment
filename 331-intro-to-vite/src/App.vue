870843f038ee2c19a93ab6fc5bb8e7a02eb99f61
Cai Jialu <2110220161@qq.com>
2024-11-07 11:21:26 +0800

    3

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -8,6 +8,7 @@ const { message } = storeToRefs(store)
 
 <template>
   <div id="layout">
+    <div class="text-center font-sans text-gray-700 antialias"></div>
     <header>
       <div id="flashMessage" v-if="message">
         <h4>{{ message }}</h4>
@@ -22,32 +23,22 @@ const { message } = storeToRefs(store)
 
     <RouterView />
   </div>
-
 </template>
 
 <style>
-#layout {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+
 
 nav {
   padding: 30px;
-
 }
 
 nav a {
   font-weight: bold;
   color: #2c3e50;
-
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
-
 }
 
 h2 {
